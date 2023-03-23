e25587207e94be8f4115e039fc2cb1b58b3b88d0
ramii989 <100424886+ramii989@users.noreply.github.com>
2023-03-23 19:57:29 -0400

    uploaded assets

---
+++ redlining /script.js
@@ -0,0 +1,9 @@
+{
+    "title": "Fear and Desire",
+        "year": 1952,
+            "alsoWriter" : false,
+                "posterImage": "https://upload.wikimedia.org/wikipedia/en/f/f7/Fear_and_Desire_Poster.jpg",
+                    "runTime": "61 minutes",
+                        "imdbLink": "https://www.imdb.com/title/tt0045758",
+                            "imdbRating": 5.3
+},
