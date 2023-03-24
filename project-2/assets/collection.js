33bb833ab3dfaa406c398aa49713656c9771441f
ramii989 <100424886+ramii989@users.noreply.github.com>
2023-03-24 01:58:59 -0400

    info updated
    push

--- project-2/assets/collection.js
+++ project-2/assets/collection.js
@@ -11,7 +11,7 @@ const renderItems = (collection) => {
 
         // You can make each element inside of that…
         const itemTitle = document.createElement('h2') // Make an `h2`
-        itemTitle.innerHTML = item.title // Put the JSON title inside
+        itemTitle.innerHTML = item.grade // Put the JSON title inside
         listItem.appendChild(itemTitle) // And add it to the `li`!
 
         const itemImage = document.createElement('img') // And an image
@@ -21,19 +21,19 @@ const renderItems = (collection) => {
 
         // This can get annoying, so we can use “template literals” instead
         const itemDetails =
-            `
-				<p>Released in <time>${item.year}</time></p>
-				<p><em>${item.runTime}</em></p>
-				<a href="${item.imdbLink}">
-					<p>${item.imdbRating} / 10 →</p>
-				</a>
+            `  <h2>${item.area}</h2>
+				<p>Infiltration of ${item.infiltration}</p>
+				<p>Occupation <em>${item.job}</em></p>
+				<p>Availability of mortgage funds are ${item.funds}</p>
+					<p>${item.description}</p>
+				
 			`
         listItem.insertAdjacentHTML('beforeend', itemDetails) // Which can we then insert
 
         // You can build logic from your data, too
-        if (!item.alsoWriter) { // If this is `false`
-            listItem.classList.add('faded') // Add this class to the whole `li`
-        }
+        //if (!item.alsoWriter) { // If this is `false`
+        //  listItem.classList.add('faded') // Add this class to the whole `li`
+        // }
 
         collectionList.appendChild(listItem) // Then add the whole `li` into the `ul`
     })
@@ -46,5 +46,5 @@ fetch('assets/collection.json')
     .then(response => response.json())
     .then(collection => {
         // And passes the data to the function, above!
-        renderItems(collection.reverse()) // In reverse order
+        renderItems(collection)
     })
