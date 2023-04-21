10f1a3ac269c1cb6d962dd3032b3a01ffca58e9e
ramii989 <100424886+ramii989@users.noreply.github.com>
2023-04-21 02:03:52 -0400

    Data from data nyc fetced

--- open-data/assets/data.js
+++ open-data/assets/data.js
@@ -30,29 +30,23 @@ const renderItems = (collection) => {
 
         // You can make each element inside of that…
         const itemTitle = document.createElement('h2') // Make an `h2`
-        itemTitle.innerHTML = item.title // Put the JSON title inside
+        itemTitle.innerHTML = item.county // Put the JSON title inside
         listItem.appendChild(itemTitle) // And add it to the `li`!
 
-        const itemImage = document.createElement('img') // And an image
-        itemImage.src = item.posterImage // Set the `src` attribute from the JSON
-        listItem.appendChild(itemImage) // And add that too
 
 
         // This can get annoying, so we can use “template literals” instead
         const itemDetails =
             `
-				<p>Released in <time>${item.year}</time></p>
-				<p><em>${item.runTime}</em></p>
-				<a href="${item.imdbLink}">
-					<p>${item.imdbRating} / 10 →</p>
-				</a>
+				<p>Report created on ${item.record_create_date}</p>
+				<p><em>${item.bias_motive_description}</em></p>
+                <p>Offense: ${item.offense_description}</p>
+				
 			`
         listItem.insertAdjacentHTML('beforeend', itemDetails) // Which can we then insert
 
         // You can build logic from your data, too
-        if (!item.alsoWriter) { // If this is `false`
-            listItem.classList.add('faded') // Add this class to the whole `li`
-        }
+
 
         collectionList.appendChild(listItem) // Then add the whole `li` into the `ul`
     })
@@ -61,7 +55,7 @@ const renderItems = (collection) => {
 
 
 // Fetch gets your JSON file…
-fetch('assets/collection.json')
+fetch(dataUrl + '?$limit=20&county=NEW YORK')
     .then(response => response.json())
     .then(collection => {
         // And passes the data to the function, above!
