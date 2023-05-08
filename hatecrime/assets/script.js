23f985406ee2731def2417f8cadd3bfdd147b874
ramii989 <100424886+ramii989@users.noreply.github.com>
2023-05-08 05:38:46 -0400

    added parse for 2020 year offenses
    Filtering for crimes against asians

--- hatecrime/assets/script.js
+++ hatecrime/assets/script.js
@@ -1,7 +1,35 @@
+console.log('______COSAS NUEVAS_____________')
 const dataUrl = 'https://data.cityofnewyork.us/resource/bqiq-cu78.json'
 
-fetch(dataUrl + '?$limit=2500')
+const parseData = (crimes) => {
+	let typeFelony = 0
+	let typeViolation = 0
+	let typeMisdemeanor = 0
+	let typeOther = 0
+
+	crimes.forEach(crime => {
+		if (crime.law_code_category_description == 'FELONY') typeFelony = typeFelony + 1
+
+		else if (crime.law_code_category_description == 'VIOLATION') typeViolation = typeViolation + 1
+		else if (crime.law_code_category_description == 'MISDEMEANOR') typeMisdemeanor = typeMisdemeanor + 1
+		else typeOther = typeOther + 1
+
+		//console.log(crime.law_code_category_description)
+	})
+
+	console.log('Felony:' + typeFelony)
+	console.log('Violation:' + typeViolation)
+	console.log('Misdemeanor:' + typeMisdemeanor)
+	console.log('Other:' + typeOther)
+
+
+	//console.log(data)
+}
+
+fetch(dataUrl + '?$limit=3000&complaint_year_number=2020&bias_motive_description=ANTI-ASIAN')
 	.then(response => response.json())
 	.then(data => {
-		console.log(data)
+		parseData(data)
 	})
+
+
