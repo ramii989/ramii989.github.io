498303f7c12e610f9f4414e15eefb7c4c11a0946
ramii989 <100424886+ramii989@users.noreply.github.com>
2023-05-08 03:08:58 -0400

    fetch data

--- hatecrime/assets/script.js
+++ hatecrime/assets/script.js
@@ -1,1 +1,7 @@
-console.log("shes working")
+const dataUrl = 'https://data.cityofnewyork.us/resource/bqiq-cu78.json'
+
+fetch(dataUrl + '?$limit=2500')
+	.then(response => response.json())
+	.then(data => {
+		console.log(data)
+	})
